a2832cdce4fc9961a6d214f707b579dabd5e5137
Nacho <45735795+TheCoderGamer1000@users.noreply.github.com>
2020-11-24 22:39:48 +0100

    Correct atribution
    Make the atribution to carpet-addon
    Little fix in performance

--- src/main/java/net/alephsmp/aleph_carpet/mixins/ServerChunkManagerMinix.java
+++ src/main/java/net/alephsmp/aleph_carpet/mixins/ServerChunkManagerMinix.java
@@ -9,11 +9,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+// This code was adapted from https://github.com/whoImT/carpet-addons
+
 @Mixin(ServerChunkManager.class)
 public class ServerChunkManagerMinix {
     @Inject(method = "shouldTickEntity", at = @At(value = "HEAD"), cancellable =  true)
     private void onShouldTickEntity(Entity entity, CallbackInfoReturnable<Boolean> cir){
-        if(entity instanceof ProjectileEntity && AlephSimpleSettings.keepProjectilesTicked)
+        if(AlephSimpleSettings.keepProjectilesTicked && entity instanceof ProjectileEntity)
             cir.setReturnValue(true);
     }
 }
